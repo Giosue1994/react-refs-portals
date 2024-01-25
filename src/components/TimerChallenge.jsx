73797e8a51a07467f537ba02a9982cf84ff1cb0e
Giosuè Lentini <63297733+Giosue1994@users.noreply.github.com>
2024-01-25 11:25:57 +0100

    impostare timer al click

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,15 +1,32 @@
+import { useState } from "react";
+
 export default function TimerChallenge({title, targetTime}) {
+
+    const [timerStarted, setTimerStarted] = useState(false);
+    const [timerExpired, setTimerExpired] = useState(false);
+
+    function handleStart() {
+        setTimeout( () => {
+            setTimerExpired(true);
+        }, targetTime * 1000 );
+
+        setTimerStarted(true);
+    }
+
     return (
       <section className="challenge">
         <h2>{title}</h2>
-        <p className="challenge-time">{targetTime} second{targetTime > 1 ? 's' : ''}</p>
-        <p>
-            <button>
-                Start Challege
-            </button>
+        {timerExpired && <p>You Lost!</p>}
+        <p className="challenge-time">
+          {targetTime} second{targetTime > 1 ? "s" : ""}
         </p>
-        <p className="">
-            Time in running... / Timer inactive
+        <p>
+          <button onClick={handleStart}>
+            {timerStarted ? "Stop" : "Start"} Challege
+          </button>
+        </p>
+        <p className={timerStarted ? "active" : undefined}>
+          {timerStarted ? 'Time in running...' : 'Timer inactive'}
         </p>
       </section>
     );

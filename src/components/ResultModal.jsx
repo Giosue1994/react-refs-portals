9224b4a6714407cc8fa05a3e346ddfb10fbe4c25
Giosuè Lentini <63297733+Giosue1994@users.noreply.github.com>
2024-01-25 17:01:29 +0100

    modale risultati (useImperativeHandle)

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,16 +1,32 @@
-import { forwardRef } from "react";
+import { forwardRef, useImperativeHandle, useRef } from "react";
 
-const ResultModal = forwardRef(function ResultModal({result, targetTime},ref) {
-    return (
-      <dialog ref={ref} className="result-modal">
-        <h2>You {result}</h2>
-        <p>The target time was <strong>{targetTime} seconds</strong>.</p>
-        <p>You stopped the time with <strong>X seconds left</strong>.</p>
-        <form method="dialog">
-          <button>Close</button>
-        </form>
-      </dialog>
-    );
+const ResultModal = forwardRef(function ResultModal({result, targetTime}, ref) {
+  const dialog = useRef();
+
+  // useImperativeHandle accetta come valori "ref" e una funzione che ritorna un 
+  // oggetto in cui inserire i metodi che devono essere esposti da quedto componente ad un altro
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal();
+      }
+    };
+  });
+
+  return (
+    <dialog ref={dialog} className="result-modal">
+      <h2>You {result}</h2>
+      <p>
+        The target time was <strong>{targetTime} seconds</strong>.
+      </p>
+      <p>
+        You stopped the time with <strong>X seconds left</strong>.
+      </p>
+      <form method="dialog">
+        <button>Close</button>
+      </form>
+    </dialog>
+  );
 })
 
 export default ResultModal;
